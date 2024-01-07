d6e8468b5539bd9234e0bb4e2e6cc284ae91d457
John Steele <j.steelecg@gmail.com>
2024-01-07 00:57:36 -0400

    add documentation about what each middleware does and move const app to line 2. End of project

--- myApp.js
+++ myApp.js
@@ -1,31 +1,55 @@
 const express = require('express');
-const helmet = require('helmet');
 const app = express();
 
+// "import helmet to file"
+const helmet = require('helmet');
+
+// Hides or Removes X-Powered-By header from the request
 app.use(helmet.hidePoweredBy());
+
+// Restricts who can place the site in a Frame, has 3 modes: DENY, SAMEORIGIN AND ALLOW-FROM
 app.use(helmet.frameguard(
   {action: 'deny'}
 ));
+
+// Basic protection from XSS attacks
 app.use(helmet.xssFilter());
+
+// Instructs browser to not bypass the provided Content-Type
 app.use(helmet.noSniff());
+
+// Prevents IE users from executing downloads in trusted site context
 app.use(helmet.ieNoOpen());
 
-const timeInSeconds = 90*24*60*60;
+// HTTP Strict Transport Security
+// Policy that protects against protocol downgrade attacks and cookie hijacking
+const timeInSeconds = 90*24*60*60; // 90 Days in seconds
 app.use(helmet.hsts(
-  {maxAge: timeInSeconds, force: true}
+  {maxAge: timeInSeconds, force: true} // force is only being used for this project
 ));
+// Note: Configuring HTTPS on a custom website requires SSL/TLS Certificates 
+// and acquisition of a Domain.
 
-app.use(helmet.dnsPrefetchControl());
+// Fetches DNS records for the links in a page, can improve performance
+// May lead to DNS service over-use, privacy issues and page statitics alteration
+app.use(helmet.dnsPrefetchControl()); // Disables Prefetch
+
+// Tries to disable caching in clients browser.
 app.use(helmet.noCache());
+
+// Prevents injections of unintended scriptsXSS vulnerabilities, undesired tracking,
+// malicious frames, and more.
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", 'trusted-cdn.com'],
+      defaultSrc: ["'self'"], // fallback directive
+      scriptSrc: ["'self'", 'trusted-cdn.com'], // Only allow scripts to be downloaded from site and specific domains.
     }
   })
 );
 
+// app.use(helmet()) automatically includes the above middleware except noCache and CSP
+// The 'parent' helmet() middleware is easy to implement in a real project
 
 
 module.exports = app;
