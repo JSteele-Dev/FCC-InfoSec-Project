884625cb9833891c4dff50b208e3a3b5dce26dca
John Steele <j.steelecg@gmail.com>
2024-01-06 23:39:31 -0400

    update typo of contentSecurityPolicy directive to directives

--- myApp.js
+++ myApp.js
@@ -19,10 +19,10 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(
   helmet.contentSecurityPolicy({
-    directive: {
+    directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'trusted-cdn.com'"],
-    },
+    }
   })
 );
 
